c6a6a45d7f5368e98e3677a957bfd5def32b7e0a
Timothy Stone <github@petmystone.com>
2021-01-12 21:21:56 -0500

    Create initial Username Token SOAP Client project
    Initial commit with clean up of project created from Spring Initializer.

---
+++ src/test/java/com/anothercaffeinatedday/ws/soap/utclient/UtclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anothercaffeinatedday.ws.soap.utclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
