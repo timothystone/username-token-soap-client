0078320219c4faba4d15e53611b681d454303321
Timothy Stone <github@petmystone.com>
2021-01-13 20:04:28 -0500

    Stub CXF WSS4J OutInterceptors
    Add the CXF WS Security implamentation and get a client for the
    paymentProcessor service.

--- src/main/java/com/anothercaffeinatedday/ws/soap/utclient/Main.java
+++ src/main/java/com/anothercaffeinatedday/ws/soap/utclient/Main.java
@@ -4,11 +4,16 @@ import com.anothercaffeinatedday.ws.soap.PaymentProcessor;
 import com.anothercaffeinatedday.ws.soap.PaymentProcessorImplService;
 import com.anothercaffeinatedday.ws.soap.PaymentProcessorRequest;
 import com.anothercaffeinatedday.ws.soap.PaymentProcessorResponse;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.endpoint.Endpoint;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.ws.security.wss4j.WSS4JOutInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Map;
 
 public class Main {
 
@@ -19,7 +24,15 @@ public class Main {
         try {
             PaymentProcessorImplService service = new PaymentProcessorImplService(new URL(WSDL_LOCATION));
             PaymentProcessor paymentProcessorImplPort = service.getPaymentProcessorImplPort();
+            Client client = ClientProxy.getClient(paymentProcessorImplPort);
+            Endpoint clientEndpoint = client.getEndpoint();
+
+            Map<String, Object> properties = null;
+            WSS4JOutInterceptor outInterceptor = new WSS4JOutInterceptor(properties);
+            clientEndpoint.getOutInterceptors().add(outInterceptor);
+
             PaymentProcessorResponse response = paymentProcessorImplPort.processPayment(new PaymentProcessorRequest());
+
             LOGGER.debug(Boolean.toString(response.isResult()));
         } catch (MalformedURLException murle) {
             murle.printStackTrace();
