66d843100054dc052e524e0dfd71d8f40c723bab
Timothy Stone <github@petmystone.com>
2021-01-16 18:46:37 -0500

    Add checkstyle to the build
    The configuration file is present. The rules pass.

--- src/main/java/com/anothercaffeinatedday/ws/soap/utclient/Main.java
+++ src/main/java/com/anothercaffeinatedday/ws/soap/utclient/Main.java
@@ -4,6 +4,10 @@ import com.anothercaffeinatedday.ws.soap.PaymentProcessor;
 import com.anothercaffeinatedday.ws.soap.PaymentProcessorImplService;
 import com.anothercaffeinatedday.ws.soap.PaymentProcessorRequest;
 import com.anothercaffeinatedday.ws.soap.PaymentProcessorResponse;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.endpoint.Endpoint;
 import org.apache.cxf.frontend.ClientProxy;
@@ -13,37 +17,43 @@ import org.apache.wss4j.dom.handler.WSHandlerConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
+/**
+ * The entry point for the payment process SOAP Client.
+ */
 public class Main {
 
-    public static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-    public static final String WSDL_LOCATION = "http://localhost:8080/javafirstws/services/paymentProcessor?wsdl";
+  public static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+  public static final String WSDL_LOCATION = "http://localhost:8080/javafirstws/services/paymentProcessor?wsdl";
 
-    public static void main(String[] args) {
-        try {
-            PaymentProcessorImplService service = new PaymentProcessorImplService(new URL(WSDL_LOCATION));
-            PaymentProcessor paymentProcessorImplPort = service.getPaymentProcessorImplPort();
-            Client client = ClientProxy.getClient(paymentProcessorImplPort);
-            Endpoint clientEndpoint = client.getEndpoint();
+  /**
+   * The payment processor SOAP client
+   *
+   * <p>A simple demonstration of statically defining and calling a service requiring WS-Security options.
+   *
+   * @param args an array of Strings passed to the runtime
+   */
+  public static void main(String[] args) {
+    try {
+      PaymentProcessorImplService service = new PaymentProcessorImplService(new URL(WSDL_LOCATION));
+      PaymentProcessor paymentProcessorImplPort = service.getPaymentProcessorImplPort();
+      Client client = ClientProxy.getClient(paymentProcessorImplPort);
 
-            Map<String, Object> properties = new HashMap<>();
-            properties.put(WSHandlerConstants.ACTION, WSHandlerConstants.USERNAME_TOKEN);
-            properties.put(WSHandlerConstants.USER, "tstone");
-            properties.put(WSHandlerConstants.PASSWORD_TYPE, WSConstants.PW_TEXT);
-            properties.put(WSHandlerConstants.PW_CALLBACK_CLASS, UTPasswordCallback.class.getName());
+      Map<String, Object> properties = new HashMap<>();
+      properties.put(WSHandlerConstants.ACTION, WSHandlerConstants.USERNAME_TOKEN);
+      properties.put(WSHandlerConstants.USER, "tstone");
+      properties.put(WSHandlerConstants.PASSWORD_TYPE, WSConstants.PW_TEXT);
+      properties.put(WSHandlerConstants.PW_CALLBACK_CLASS, UtPasswordCallback.class.getName());
 
-            WSS4JOutInterceptor outInterceptor = new WSS4JOutInterceptor(properties);
-            clientEndpoint.getOutInterceptors().add(outInterceptor);
+      WSS4JOutInterceptor outInterceptor = new WSS4JOutInterceptor(properties);
+      Endpoint clientEndpoint = client.getEndpoint();
+      clientEndpoint.getOutInterceptors().add(outInterceptor);
 
-            PaymentProcessorResponse response = paymentProcessorImplPort.processPayment(new PaymentProcessorRequest());
+      PaymentProcessorResponse response = paymentProcessorImplPort.processPayment(new PaymentProcessorRequest());
 
-            LOGGER.debug(Boolean.toString(response.isResult()));
-        } catch (MalformedURLException murle) {
-            murle.printStackTrace();
-        }
+      LOGGER.debug(Boolean.toString(response.isResult()));
+    } catch (MalformedURLException murle) {
+      murle.printStackTrace();
     }
+  }
 }
---
+++ src/main/java/com/anothercaffeinatedday/ws/soap/utclient/UtPasswordCallback.java
@@ -0,0 +1,24 @@
+package com.anothercaffeinatedday.ws.soap.utclient;
+
+import java.io.IOException;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import org.apache.wss4j.common.ext.WSPasswordCallback;
+
+/**
+ * The User Token Password Callback for the SOAP Client.
+ *
+ */
+public class UtPasswordCallback implements CallbackHandler {
+  @Override
+  public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+    for (Callback callback : callbacks) {
+      WSPasswordCallback passwordCallback = (WSPasswordCallback) callback;
+      if (passwordCallback.getIdentifier().equals("tstone")) {
+        passwordCallback.setPassword("tstone");
+        return;
+      }
+    }
+  }
+}
